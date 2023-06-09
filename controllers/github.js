1e06eb866290ea0373de601e6944ad0a16eae3f8
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2023-06-09 14:06:22 +0530

    feat(puzzle) : controller added for github actions

--- controllers/github.js
+++ controllers/github.js
@@ -1,1 +1,11 @@
-module.exports.post = async (req, res) => {};
+const { log } = require('@spaship/common/lib/logging/pino');
+const { github } = require('../config');
+const { githubPullRequestOnOpen, githubPullRequestOnCloseAndMerge } = require('../services/github/github');
+
+module.exports.post = async (req, res) => {
+  const payload = req.body;
+  log.info(payload);
+  if (payload.action === github.PR_OPENED && payload.pull_request.state === github.STATE_OPEN) await githubPullRequestOnOpen(payload.action, payload);
+  if (payload.action === github.PR_CLOSED && payload.pull_request.merged) await githubPullRequestOnCloseAndMerge(payload.action, payload);
+  res.sendStatus(200);
+};
