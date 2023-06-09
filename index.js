5e25f93e1b20b6c7f0ea80ec3b6412e34045b8f5
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2023-06-09 11:25:42 +0530

    chore(puzzle) : port and ASCII log added

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const { log } = require('@spaship/common/lib/logging/pino');
+const { loadFiles } = require('@spaship/common/lib/logging/pino-config');
 const app = require('./app');
 const config = require('./config');
 const pkgJSON = require('./package.json');
 
+console.info(`
+███████╗██████╗  █████╗ ███████╗██╗  ██╗██╗██████╗     ██████╗ ██╗   ██╗███████╗███████╗██╗     ███████╗
+██╔════╝██╔══██╗██╔══██╗██╔════╝██║  ██║██║██╔══██╗    ██╔══██╗██║   ██║╚══███╔╝╚══███╔╝██║     ██╔════╝
+███████╗██████╔╝███████║███████╗███████║██║██████╔╝    ██████╔╝██║   ██║  ███╔╝   ███╔╝ ██║     █████╗  
+╚════██║██╔═══╝ ██╔══██║╚════██║██╔══██║██║██╔═══╝     ██╔═══╝ ██║   ██║ ███╔╝   ███╔╝  ██║     ██╔══╝  
+███████║██║     ██║  ██║███████║██║  ██║██║██║         ██║     ╚██████╔╝███████╗███████╗███████╗███████╗
+╚══════╝╚═╝     ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝╚═╝         ╚═╝      ╚═════╝ ╚══════╝╚══════╝╚══════╝╚══════╝                                                                                                     
+`)
+
 if (config.env === 'production') {
-  log.info(`Starting SPAship Puzzle ${pkgJSON.version} running on production.`);
+  log.info(`Starting SPAship Puzzle ${pkgJSON.version} running on production`);
 } else {
-  log.info(`Starting SPAship Puzzle ${pkgJSON.version} running.`);
+  log.info(`Starting SPAship Puzzle ${pkgJSON.version} running on ${config.env}`);
 }
 
 (async () => {
-  app.listen(9191, () => {
-    log.info(`Server started for ${config.env}!`);
+  app.listen(config.port, () => {
+    log.info(`Server started for ${config.env} on port ${config.port}`);
   });
 })();
