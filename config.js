5e94b413ecfac250de7f59a2d313458600c68eb1
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2023-06-09 14:05:08 +0530

    chore(puzzle) : new configs added into puzzle

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-module.exports = {
+module.exports.config = {
   env: process.env.NODE_ENV || 'dev',
   port: process.env.ORCHESTRATOR_SECRET || 9191,
   githubAccessToken: process.env.GITHUB_ACCESS_TOKEN,
@@ -7,4 +7,12 @@ module.exports = {
   gitlabWebhookSecret: process.env.GITLAB_WEBHOOK_SECRET,
   orchestratorBaseUrl: process.env.ORCHESTRATOR_BASE_URL,
   orchestratorSecret: process.env.ORCHESTRATOR_SECRET
+};
+
+module.exports.github = {
+  PR_OPENED: 'opened',
+  PR_CLOSED: 'closed',
+  PR_MERGED: 'merged',
+  STATE_OPEN: 'open',
+  COMMENT_CREATED: 'created'
 };
