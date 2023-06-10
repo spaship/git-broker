c615c41f99f579c9696ffcd16ce40e5189f3ac3f
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2023-06-10 22:45:20 +0530

    feat(puzzle) : gitlab services added

---
+++ services/gitlab/api.js
@@ -0,0 +1,134 @@
+const { log } = require('@spaship/common/lib/logging/pino');
+const { config, gitlab, deployment } = require('../../config');
+const axios = require('axios');
+
+const getGitlabHeaders = () => {
+  return { 'PRIVATE-TOKEN': config.gitlabAccessToken, 'Content-Type': 'application/json' };
+};
+
+const fetchCommentsFromGitlab = async (projectId, mergeRequestId) => {
+  const deploymentEnvs = new Set();
+  try {
+    const response = await axios.get(`${gitlab.BASE_URL}/${projectId}/merge_requests/${mergeRequestId}/notes`, { headers: getGitlabHeaders() });
+    const comments = response.data;
+    comments.forEach((comment) => {
+      const commentBody = comment.body.toLowerCase();
+      if (commentBody.includes(deployment.SPECIFIER)) {
+        const matches = commentBody.match(deployment.ENVS_REGEX);
+        matches.forEach((env) => {
+          deploymentEnvs.add(env);
+        });
+      }
+    });
+    if (deploymentEnvs.size) log.info(`To be Deployed in ${[...deploymentEnvs]}`);
+    else log.info('No environment found for deployment');
+  } catch (error) {
+    log.error(`Error in fetchCommentsFromGitlab`);
+    log.error(error);
+  }
+  return deploymentEnvs;
+};
+
+const commentOnGitlabMergeRequest = async (payload, projectId, mergeRequestId, commentBody) => {
+  try {
+    await axios.post(
+      `${gitlab.BASE_URL}/${projectId}/merge_requests/${mergeRequestId}/notes`,
+      { body: commentBody },
+      { headers: getGitlabHeaders() }
+    );
+    log.info(getRepoDetails(payload));
+    log.info(`Commented on Pull Request ${mergeRequestId} Successfully`);
+  } catch (error) {
+    log.error('Error in commenting on Specific PR: ', error);
+  }
+};
+
+const commentOnGitlabMergedCommit = async (payload, projectId, commitSha, commentBody) => {
+  try {
+    await axios.post(
+      `${gitlab.BASE_URL}/${projectId}/repository/commits/${commitSha}/comments`,
+      { note: commentBody },
+      { headers: getGitlabHeaders() }
+    );
+    log.info(getRepoDetails(payload));
+    log.info(`Success in commenting on Specific commit : ${commitSha}`);
+  } catch (error) {
+    log.error('Error in commentOnGitlabMergedCommit');
+    log.error(error);
+  }
+};
+
+const createNewBranchOnGitlabRepository = async (payload, projectId, newRef, targetBranch) => {
+  try {
+    await axios.post(
+      `${gitlab.BASE_URL}/${projectId}/repository/branches`,
+      {
+        branch: newRef,
+        ref: targetBranch
+      },
+      { headers: getGitlabHeaders() }
+    );
+    log.info(getRepoDetails(payload));
+    log.info(`Successfully created new branch : ${newRef}`);
+  } catch (error) {
+    log.error('Error in createNewBranchOnGithubRepository');
+    log.error(error);
+  }
+};
+
+const createFileOnGitlabRepository = async (payload, projectId, commentBody, filePath, newRef) => {
+  try {
+    await axios.post(
+      `${gitlab.BASE_URL}/${projectId}/repository/files/${encodeURIComponent(filePath)}`,
+      {
+        branch: newRef,
+        content: commentBody,
+        commit_message: 'feat(spaship) : spaship.yaml added'
+      },
+      { headers: getGitlabHeaders() }
+    );
+    log.info(getRepoDetails(payload));
+    log.info('File created successfully on Gitlab');
+  } catch (error) {
+    log.error('Error in createFileOnGitlabRepository');
+    log.error(error);
+  }
+};
+
+const alterFileOnGitlabRepository = async (payload, projectId, commentBody, filePath, newRef) => {
+  try {
+    const response = await axios.put(
+      `${gitlab.BASE_URL}/${projectId}/repository/files/${encodeURIComponent(filePath)}`,
+      {
+        branch: newRef,
+        content: commentBody,
+        commit_message: 'chore(spaship) : spaship.yaml updated'
+      },
+      { headers: getGitlabHeaders() }
+    );
+    log.info(getRepoDetails(payload));
+    log.info('File updated successfully on Gitlab');
+  } catch (error) {
+    log.error('Error in alterFileOnGitlabRepository');
+    log.error(error);
+  }
+};
+
+const getRepoDetails = (payload) => {
+  return {
+    repository: payload.repository.name,
+    pullRequestNumber: payload.object_attributes.iid,
+    headRef: payload.object_attributes.target_branch,
+    baseRef: payload.object_attributes.source_branch,
+    owner: payload.user.name
+  };
+};
+
+module.exports = {
+  fetchCommentsFromGitlab,
+  commentOnGitlabMergeRequest,
+  commentOnGitlabMergedCommit,
+  createNewBranchOnGitlabRepository,
+  createFileOnGitlabRepository,
+  alterFileOnGitlabRepository
+};
---
+++ services/gitlab/index.js
@@ -0,0 +1,65 @@
+const { log } = require('@spaship/common/lib/logging/pino');
+const { v4: uuidv4 } = require('uuid');
+const { deployment } = require('../../config');
+const {
+  commentOnGitlabMergeRequest,
+  fetchCommentsFromGitlab,
+  commentOnGitlabMergedCommit,
+  createNewBranchOnGitlabRepository,
+  createFileOnGitlabRepository,
+  alterFileOnGitlabRepository
+} = require('./api');
+
+const gitlabMergeRequest = async (payload) => {
+  const projectId = payload.project.id;
+  const mergeRequestId = payload.object_attributes.iid;
+  const commentBody = `Kindly specify the names of env you want to specify in the given format [dev,stage,qa]`;
+  await commentOnGitlabMergeRequest(projectId, mergeRequestId, commentBody);
+};
+
+const gitlabMergeRequestOnCloseAndMerge = async (payload) => {
+  const mergeRequestId = payload.object_attributes.iid;
+  const projectId = payload.project.id;
+  const deploymentEnvs = await fetchCommentsFromGitlab(projectId, mergeRequestId);
+  if (!deploymentEnvs.size) return;
+  const envs = Array.from(deploymentEnvs);
+  const contextDir = payload.object_attributes.source.change_path || '/';
+  try {
+    const orchestratorPayload = createOrchestratorPayload(payload, contextDir, envs);
+    await orchestratorRequest(orchestratorPayload);
+    // @internal comment on specific Merge Request
+    await commentOnGitlabMergeRequest(
+      payload,
+      projectId,
+      mergeRequestId,
+      `Deployment started for ${[...envs]}. Please check SPAship Manager for more details.`
+    );
+    // @internal git operations [TBD use-cases]
+    // await gitlabOperations(payload);
+  } catch (error) {
+    log.error('Error in createFileOnGitlabRepository');
+    log.error(error);
+  }
+};
+
+const gitlabOperations = async (payload) => {
+  const filePath = 'spaship.yaml';
+  const commentBody = `## Deployed by SPAship Puzzle`;
+  const projectId = payload.project.id;
+  const newRef = `${deployment.SPECIFIER}-${uuidv4().substring(0, 5)}`;
+  const targetBranch = payload.object_attributes.target_branch;
+  const commitSha = payload.object_attributes.last_commit.id;
+  // @internal comment on a specific commit
+  await commentOnGitlabMergedCommit(payload, projectId, commitSha, commentBody);
+  // @internal create a new branch
+  await createNewBranchOnGitlabRepository(payload, projectId, newRef, targetBranch);
+  // @internal create a new file in the repository
+  await createFileOnGitlabRepository(payload, projectId, commentBody, filePath, newRef);
+  // @internal alter an existing file in the repository
+  await alterFileOnGitlabRepository(payload, projectId, commentBody, filePath, newRef);
+};
+
+module.exports = {
+  gitlabMergeRequest,
+  gitlabMergeRequestOnCloseAndMerge
+};
