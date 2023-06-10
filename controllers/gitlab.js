635e1291495eb5bb441a7c0ae3114dcb6979d503
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2023-06-10 22:43:58 +0530

    feat(puzzle) : gitlab controller added

--- controllers/gitlab.js
+++ controllers/gitlab.js
@@ -1,1 +1,11 @@
-module.exports.post = async (req, res) => {};
+const { gitlab } = require('../config');
+const { gitlabMergeRequest, gitlabMergeRequestOnCloseAndMerge } = require('../services/gitlab');
+
+module.exports.post = async (req, res) => {
+  const action = req.header('X-Gitlab-Event');
+  const payload = req.body;
+  if (action === gitlab.MERGE_REQUEST && payload?.object_attributes?.state === gitlab.STATE_OPENED) await gitlabMergeRequest(payload);
+  else if (payload?.object_attributes?.state === gitlab.STATE_MERGED || payload?.object_attributes?.state === gitlab.STATE_CLOSED)
+    await gitlabMergeRequestOnCloseAndMerge(payload);
+  res.sendStatus(200);
+};
