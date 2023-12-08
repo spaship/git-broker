797799e781bd7c5f7787f7e1787610809226f22b
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2023-12-09 00:51:46 +0530

    feat : lighthouse service added

--- routes/events.js
+++ routes/events.js
@@ -2,10 +2,13 @@ const { Router } = require('express');
 const events = require('../controllers/events');
 const auth = require('../middlewares/auth');
 const router = new Router();
-
+const lighthouse = require('../controllers/lighthouse');
 /**
  * @internal webhook for gitlab
  */
 router.post('/comment', events.post);
+
+
+router.get('/lighthouse', lighthouse.launchChromeAndRunLighthouse);
 
 module.exports = router;
